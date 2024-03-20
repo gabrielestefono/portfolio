14dac2b4dda5ad5a5c6af2f76639df23b2fe5565
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-03-20 10:05:25 -0300

    Criando apresentação

--- components/index/apresentacao.tsx
+++ components/index/apresentacao.tsx
@@ -1,6 +1,24 @@
+import Link from "next/link";
+
 export default function Apresentacao() {
 	return (
 		<div>
+			<div>
+				<div>
+					<div>
+						<h1>Olá! Eu sou</h1>
+					</div>
+					<h2>Desenvolvedor Full Stack</h2>
+					<p>Um desenvolvedor full stack é um profissional versátil que possui habilidades tanto no desenvolvimento do lado do cliente (front-end) quanto no lado do servidor (back-end) de aplicações web. Sua capacidade de trabalhar em ambas as áreas os torna ativos valiosos em equipes de desenvolvimento, pois podem abordar uma ampla gama de desafios de programação em um projeto. Eles são como "poliglotas" da programação, dominando várias linguagens e tecnologias para criar aplicativos web completos e funcionais.</p>
+					<Link href="">Veja meus Projetos</Link>
+				</div>
+				<div>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+			</div>
 		</div>
 	);
 }
